d5af0377ebaeb8be9cfbbf4f3c6b5268bfcb18ec
nopbyte <nopbyte@users.noreply.github.com>
2017-02-02 14:44:26 +0000

    update

--- agile-osjs-install.sh
+++ agile-osjs-install.sh
@@ -1,13 +1,12 @@
-git clone https://github.com/doitdagi/OS.js
+#git clone https://github.com/doitdagi/OS.js
 cd OS.js
 git checkout 3e9a9e4dcd72e8b4cc666e57e0088c675c449062
-npm install --save git@https://github.com/Agile-IoT/agile-idm-client#v1.0.0
-npm install
+#npm install
 #npm install --production
 grunt create-handler:Agile
 #put handlers!
-cp ./src/client/javascript/handlers/agile/handler.js  ./src/client/javascript/handlers/agile/handler.js
-cp ./src/server/node/handlers/agile/handler.js ./src/server/node/handlers/agile/handler.js
+cp ../src/client/javascript/handlers/agile/handler.js  ./src/client/javascript/handlers/agile/handler.js
+cp ../src/server/node/handlers/agile/handler.js ./src/server/node/handlers/agile/handler.js
 #set agile handler
 grunt config --set=handler --value=agile
 #Configure AGILE IDM part
@@ -15,7 +14,7 @@ grunt config --set=handler --value=agile
 grunt config --set=server.handlers.agile.idm --value=http://agilegw.local:3000/oauth2/api/userinfo
 grunt config -set=client.Connection.RedirectIDM --value=http://agilegw.local:3000/oauth2/dialog/authorize/
 #grunt config -set=client.Oauth2.id --value=OSjs
-grunt config -set=client.Oauth2.id --value=OSJS
-grunt config --set=server.handlers.agile.tokenFile --value=/tmp/token.txt
+grunt config -set=client.Oauth2.id --value=AGILE-OSJS
+
 
 grunt
